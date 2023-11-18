1b2f8d032e18256481996a67454c27d20d3daa3c
haticesaike <haticesaike374@gmail.com>
2023-11-18 22:23:27 +0300

    addHomePage

--- burger/src/components/Home.jsx
+++ burger/src/components/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+import background from "../assets/burgerbackground.jpg";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div
+      className="homeContainer"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <Link to="/menu">
+        <button className="orderButton">Order Now</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
