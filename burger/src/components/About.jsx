1fc8ceed92ae0e0bc646eed9f13c9a51a97e8310
haticesaike <haticesaike374@gmail.com>
2023-11-20 01:17:38 +0300

    addAboutPage

--- burger/src/components/About.jsx
+++ burger/src/components/About.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "../styles/About.css";
+import banner from "../assets/banneryeni.jpg";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${banner})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>ABOUT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          itaque repudiandae illo dignissimos voluptates blanditiis quis.
+          Laborum natus, expedita delectus eligendi provident vitae repellendus
+          nobis in eum tenetur voluptas excepturi. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Nesciunt itaque repudiandae illo
+          dignissimos voluptates blanditiis quis. Laborum natus, expedita
+          delectus eligendi provident vitae repellendus nobis in eum tenetur
+          voluptas excepturi. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Nesciunt itaque repudiandae illo dignissimos
+          voluptates blanditiis quis. Laborum natus, expedita delectus eligendi
+          provident vitae repellendus nobis in eum tenetur voluptas excepturi.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
